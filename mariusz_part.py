55a6caa1bc433719a1fb0732583aba65f4c66011
Mariusz Trela <mariusztrela01@gmail.com>
2019-01-26 17:20:02 +0000

    Final equations check!

--- mariusz_part.py
+++ mariusz_part.py
@@ -2,7 +2,20 @@
 from latex2sympy.process_latex import process_sympy
 import sympy
 
-eq = process_sympy("\cos s + 1 = 0")
-print(eq)
-res = sympy.solveset(eq)
-print(res)
+def check_equations(latex_equations):
+	equations = [process_sympy(i) for i in latex_equations]
+	solution_sets = [sympy.solveset(i, domain=sympy.S.Complexes) for i in equations]
+	for i in range(len(solution_sets)):
+		if(solution_sets[i]!=solution_sets[0]):
+			print "You have a mistake in line ", i+1, "!"
+			print "The line is "
+			print latex_equations[i]
+			return
+	print "No mistakes!"
+
+
+
+
+#testing
+#check_equations(["x^2+5x+6=0", "x^2+5x=	-6", "(x+2)(x+3)=0"])
+#check_equations(["x=\sqrt{2}", "x^2-\sqrt{8}x+2"])
