d1d102729b7767013a33227cc436cb9a0035da25
Robert Hönig <robhoenig@gmail.com>
2019-01-26 22:50:16 +0000

    Update app.py
    Relatively stable, prettier GUI, supports integrals.

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 
-import kivy
 from kivy.app import App
 from kivy.uix.camera import Camera
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.uix.image import Image
+
+from kivy.core.window import Window
+Window.clearcolor = (1, 1, 1, 1)
+Window.size = (1023, 1267)
 
 ################ equation.py
 import sys
@@ -25,71 +30,105 @@ def image_to_latex(file_path):
     return latex
 ################
 
+################
+from sympy import *                                                                   
+
+def sympy_equation_to_png(equation, filename):
+    preview(equation, viewer='file', filename=filename)
+################
+
 ################ split latex into latex equations
 def split_into_equations(s):
-	#print s
-	return s[27:-20].split("\\\\")
+    return s[27:-20].split("\\\\")
 
-from latex2sympy.process_latex import process_sympy
+from process_latex import process_sympy
 import sympy
 
-def check_equations(latex_equations):
-	for i in latex_equations:
-		print i[2:-2]
-	equations = [process_sympy(i[2:-2]) for i in latex_equations]
-	for i in equations:
-		print i
-	solution_sets = [sympy.solveset(i, domain=sympy.S.Complexes) for i in equations]
-	return [ i==solution_sets[0] for i in solution_sets]
+# def check_equations_and_generate_pngs(latex_equations):
+#     equations = [process_sympy(i[2:-2]) for i in latex_equations]
+#     for i, equation in enumerate(equations):
+#         sympy_equation_to_png(equation, "output"+str(i)+".png")
+#     solution_sets = [sympy.solveset(i, domain=sympy.S.Complexes) for i in equations]
+#     return [ i==solution_sets[0] for i in solution_sets]
+
+def check_equations_and_generate_pngs(latex_equations):
+    # for i in latex_equations:
+        # print i[2:-2]
+    equations_raw = [process_sympy(i[2:-2]) for i in latex_equations]
+    for i, equation in enumerate(equations_raw):
+        sympy_equation_to_png(equation, "output"+str(i)+".png")
+    equations = [sympy.simplify((i.args[0]-i.args[1]).doit()) for i in equations_raw]
+    for i in equations:
+        print i, sympy.solveset(i, domain=sympy.S.Complexes)
+    solution_sets = [sympy.solveset(i, domain=sympy.S.Complexes) for i in equations]
+    return [True]+[solution_sets[i]==solution_sets[i-1] for i in range(1, len(solution_sets))]
 ################
  
 
 class Output(GridLayout):
-    def __init__(self, latex_equations):
-        super(Output, self).__init__()
-        self.cols = 2
-        checked_equations = check_equations(latex_equations)
+    def __init__(self, latex_equations, **kwargs):
+        super(Output, self).__init__(**kwargs)
+        self.cols = 3
+        # try:
+        checked_equations = check_equations_and_generate_pngs(latex_equations)
+        # except:
+            # self.add_widget(Label(text="Ooops! I didn't understand that input.", color=[1,0,0, 1], font_size='60dp'))
+            # return
         print checked_equations
-        for latex_equation, checked_equation in zip(latex_equations, checked_equations):
-            self.add_widget(Label(text=latex_equation))
-            if checked_equation == True:
-                self.add_widget(Label(text="True"))
+        for i, (latex_equation, checked_equation) in enumerate(zip(latex_equations, checked_equations)):
+            if i == 0:
+                self.add_widget(Label(text="Original equation:", color=[0,0,0, 1], font_size='30dp'))
             else:
-                self.add_widget(Label(text="False"))
+                # self.add_widget(Label(text="Step "+str(i)+":",  color=[0,0,0, 1], font_size='30dp'))
+                if checked_equation == True:
+                    self.add_widget(Image(source="arrow_green.jpg"))
+                else: 
+                    self.add_widget(Image(source="arrow_red.jpg"))
+
+            self.add_widget(Image(source="output"+str(i)+".png", size_hint_x=0.8, size_hint_y=0.8, allow_stretch=True))
+
+            if checked_equation == True:
+                text = "[b]This step is correct![/b]"
+                if i == 0:
+                    text = ""                    
+                self.add_widget(Label(text=text, color=[0,1,0, 1], font_size='30dp', markup=True))
+            else:
+                self.add_widget(Label(text="[b]This step went wrong![/b]", color=[1,0,0, 1], font_size='30dp', markup=True))
 
 
 class CameraExample(App):
 
     def build(self):
-        self.layout = BoxLayout(orientation='vertical')
+        self.clicked = False
+        self.layout = FloatLayout(orientation='vertical')
         # Create a camera object
-        self.cameraObject            = Camera(play=False)
+        self.cameraObject            = Camera(play=False, pos=(0,346))
         self.cameraObject.play       = True
-        self.cameraObject.resolution = (600, 600) # Specify the resolution
+        self.cameraObject.resolution = (1000, 600) # Specify the resolution
         # Create a button for taking photograph
-        self.cameraClick = Button(text="Take Photo and Evaluate")
-        self.cameraClick.size_hint=(.5, .2)
-        self.cameraClick.pos_hint={'x': .25, 'y':.75}
+        self.cameraClick = Button(text="Take Photo and Evaluate", pos=(0, 440), font_size='60dp')
+        self.cameraClick.size_hint=(1, .2)
         # bind the button's on_press to onCameraClick
         self.cameraClick.bind(on_press=self.onCameraClick)
         # add camera and button to the layout
         self.layout.add_widget(self.cameraObject)
         self.layout.add_widget(self.cameraClick)
-
-        # MY CODE
-        
-        # return the root widget
         return self.layout
+
 
     # Take the current frame of the video as the photo graph       
     def onCameraClick(self, *args):
+        if self.clicked == True:
+            self.layout.remove_widget(self.output)
+        self.clicked = True
         self.cameraObject.export_to_png('output.png')
         latex_string = image_to_latex('output.png')
-        #latex_string = "\left. \begin{array} { l } { x ^ { 2 } + 5 x + 6 = 0 } \\ { 3 x + 2 x + x ^ { 2 } + 3 x = 3 } \\ { x ^ { 2 } = 3 x + 2 x + 3 } \end{array} \right."
-        print latex_string
+        print latex_string.__repr__()
+        # latex_string = "\left. \begin{array} { l } { x ^ { 2 } + 5 x + 6 = 0 } \\ { 3 x + 2 x + x ^ { 2 } + 3 x = 3 } \\ { x ^ { 2 } = -8 x + 3 } \end{array} \right."
+        # latex_string = "\left. \begin{array} { l } { x ^ { 2 } + 5 x + 6 = 0 } \\ { 3 x + 2 x + x ^ { 2 } + 3 x = 3 } \\ { x ^ { 2 } = 3 x + 2 x + 3 } \\ { 6 = x ^ { 2 } + 4 + 5 } \end{array} \right."
         latex_equations = split_into_equations(latex_string)
         print latex_equations
-        self.output = Output(latex_equations)
+        self.output = Output(latex_equations, size_hint=(1, .35))
         self.layout.add_widget(self.output)
 
 # Start the Camera App
